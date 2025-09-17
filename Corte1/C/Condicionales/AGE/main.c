60e9b3b4f5ffdb03f5323a48eb24d6d34632930c
jl040812 <jldiazb@udistrital.edu.co>
2025-09-17 09:12:54 -0500

    Add files via upload

---
+++ Corte1/C/Condicionales/AGE/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int age = 17;
+	
+	if (age >=18){
+		printf("You are elegible to vote.\n");
+	}
+	else {
+		printf("You are not yet elegible to vote:\n");
+	}
+	return 0;
+}
